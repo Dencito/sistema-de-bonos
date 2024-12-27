0342f9384ea8608fbf537cb1cde47e7fa623f33d
Alberto-Arcentales <alberto.a@embluemail.com>
2024-12-27 11:48:32 -0300

    Display domain column and reusable CustomTable component

--- resources/js/Pages/Companies/index.jsx
+++ resources/js/Pages/Companies/index.jsx
@@ -5,11 +5,66 @@ import ModalViewBranchesCompany from '@/Components/Companies/ModalViewBranchesCo
 import ModalViewCompany from '@/Components/Companies/ModalViewCompany';
 import AuthenticatedLayout from '@/Layouts/AuthenticatedLayout';
 import { Head } from '@inertiajs/react';
-import { Table } from 'antd';
-import { MobileButton } from '@/Components/MobileButton';
+import { CustomTable } from '@/components-v2/CustomTable';
+import MobileButton from '@/Components/MobileButton';
 import FilterModal from '@/Components/Companies/FilterModal';
-const { Column } = Table;
 
+const columns = [
+    {
+        title: "Nombre",
+        dataIndex: "name",
+        key: "id"
+    },
+    {
+        title: "Dominio",
+        dataIndex: "domain",
+        key: "domain"
+    },
+    {
+        title: "Rut",
+        key: "rutVerification",
+        render: (_, record) => `${record.rutNumbers}-${record.rutDv}`
+    },
+    {
+        title: "Max. sucursales",
+        dataIndex: "max_branches",
+        key: "max_branches"
+    },
+    {
+        title: "Sucursales",
+        key: "branches",
+        render: (_, company) => (
+            <div className="flex flex-wrap gap-3">
+                <ModalViewBranchesCompany data={company} />
+                <ModalViewCompany data={company} />
+                <ModalEditCompany data={company} />
+                <ModalDeleteCompany data={company} />
+            </div>
+        )
+    },
+    {
+        title: "Estado",
+        key: "state",
+        render: (_, company) => (
+            <div className={`${company?.state?.name === 'Activo' && 'bg-green-300' ||
+                company?.state?.name === 'Inactivo' && 'bg-red-200' ||
+                company?.state?.name === 'En revisión' && 'bg-orange-300' ||
+                company?.state?.name === 'Borrado' && 'bg-red-400'
+                } font-bold rounded-full text-center p-1 w-6 h-6`}></div>
+        )
+    },
+    {
+        title: "Acciones",
+        key: "actions",
+        render: (_, company) => (
+            <div className='flex flex-wrap gap-3'>
+                <ModalViewCompany data={company} />
+                <ModalEditCompany data={company} />
+                <ModalDeleteCompany data={company} />
+            </div>
+        )
+    }
+];
 
 export default function CompanyPage({ auth, companies, states, filters }) {
     return (
@@ -33,38 +88,11 @@ export default function CompanyPage({ auth, companies, states, filters }) {
                             <FilterModal filters={filters} states={states}/>
                             <ModalCreateCompany />
                         </div>
-                        <Table className='overflow-auto' dataSource={companies.map(company => ({ ...company, key: company.id }))}>
-                            <Column title="Nombre" dataIndex="name" key="id" />
-                            <Column title="Rut" dataIndex="rutNumbers" render={(_, record) => `${record.rutNumbers}-${record.rutDv}`} key="rutVerification" />
-                            <Column title="Max. sucursales" dataIndex="max_branches" key="max_branches" />
-                            <Column
-                                title="Sucursales"
-                                key="branches"
-                                render={(_, company) => (
-                                    <div className='flex flex-wrap gap-3'>
-                                        <ModalViewBranchesCompany company={company.name} data={company.branches} />
-                                    </div>
-                                )}
-                            />
-                            <Column title="Estado" key="state" render={(_, company) => (
-                                <div className={`${company?.state?.name === 'Activo' && 'bg-green-300' ||
-                                    company?.state?.name === 'Inactivo' && 'bg-red-200' ||
-                                    company?.state?.name === 'En revisión' && 'bg-orange-300' ||
-                                    company?.state?.name === 'Borrado' && 'bg-red-400'
-                                    } font-bold rounded-full text-center p-1 w-6 h-6`}></div>
-                            )} />
-                            <Column
-                                title="Acciones"
-                                key="actions"
-                                render={(_, company) => (
-                                    <div className='flex flex-wrap gap-3'>
-                                        <ModalViewCompany data={company} />
-                                        <ModalEditCompany data={company} />
-                                        <ModalDeleteCompany data={company} />
-                                    </div>
-                                )}
-                            />
-                        </Table>
+                        <CustomTable 
+                            dataSource={companies.map(company => ({ ...company, key: company.id }))}
+                            columns={columns}
+                            scroll={{ x: true }}
+                        />
                     </div>
                 </div>
             </div>

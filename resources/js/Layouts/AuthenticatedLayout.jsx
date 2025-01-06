441e44a641cad372610ba37c8788ed4079a43811
Dencito <denarpadilla.lionel@gmail.com>
2025-01-05 23:21:36 -0300

    FEAT - add visible links for primary app

--- resources/js/Layouts/AuthenticatedLayout.jsx
+++ resources/js/Layouts/AuthenticatedLayout.jsx
@@ -1,29 +1,43 @@
-import { Links } from '@components/Links';
-import { roleDisplayNames } from '@/Utils/constants';
+import { Links } from "@components/Links";
+import { roleDisplayNames } from "@/Utils/constants";
+import { VITE_PRIMARY_SUBDOMAIN } from "@/Utils/env";
 
 export default function Authenticated({ auth, user, role, children }) {
-    const { roles } = auth
-    const nameCompanySelect = window.localStorage.getItem('companySelect');
+    const { roles } = auth;
+    const nameCompanySelect = window.localStorage.getItem("companySelect");
+    const isPrimarySubdomain = VITE_PRIMARY_SUBDOMAIN === "tickets";
 
     return (
-        <div className='flex h-screen'>
+        <div className="flex h-screen">
             <aside className="hidden h-screen w-64 bg-white p-4 shadow-md lg:block">
                 <nav className="space-y-2">
                     <div className="ps-5">
-                        <span className='font-semibold'>Empresa:</span>
-                        <p className='mb-3 text-xl font-bold uppercase'>{nameCompanySelect}</p>
-                        <span className='font-semibold'>Usuario:</span>
-                        <p className='mb-3 text-lg font-bold uppercase'>{user.username}</p>
-                        <span className='font-semibold'>Rol:</span>
-                        <p className='mb-3 text-lg font-bold uppercase'>{roleDisplayNames[role]}</p>
+                        {isPrimarySubdomain ? (
+                            <h1 className="text-2xl font-bold">
+                                USUARIO DUEÑO
+                            </h1>
+                        ) : (
+                            <div>
+                                <span className="font-semibold">Empresa:</span>
+                                <p className="mb-3 text-xl font-bold uppercase">
+                                    {nameCompanySelect}
+                                </p>
+                                <span className="font-semibold">Usuario:</span>
+                                <p className="mb-3 text-lg font-bold uppercase">
+                                    {user.username}
+                                </p>
+                                <span className="font-semibold">Rol:</span>
+                                <p className="mb-3 text-lg font-bold uppercase">
+                                    {roleDisplayNames[role]}
+                                </p>
+                            </div>
+                        )}
                         <hr />
                     </div>
                     <Links role={role} roles={roles} />
                 </nav>
             </aside>
-            <main
-                className="flex flex-1 px-3 flex-col overflow-auto"
-            >
+            <main className="flex flex-1 px-3 flex-col overflow-auto">
                 {children}
             </main>
         </div>

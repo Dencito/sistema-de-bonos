8cff2c5a0acf6919ab9bfd4fc169251b1794bb47
Dencito <denarpadilla.lionel@gmail.com>
2025-06-14 21:12:16 -0300

    feat: add error message handling to login page using errorMsg utility

--- resources/js/Pages/Auth/Login.jsx
+++ resources/js/Pages/Auth/Login.jsx
@@ -2,7 +2,7 @@ import { useEffect } from 'react';
 import { Form, Input, Checkbox, Button, Alert } from 'antd';
 import GuestLayout from '@/Layouts/GuestLayout';
 import { Head, Link, router, useForm } from '@inertiajs/react';
-import { useMessage } from '@contexts/MessageShow';
+import { useMessage, errorMsg } from '@contexts/MessageShow';
 import { roleNames } from '@/Utils/constants';
 import { authService } from '@/Services/api';
 

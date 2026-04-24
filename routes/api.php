057795a81cefb5b60bbcc8c901c2a5fafa36966b
Dencito <denarpadilla.lionel@gmail.com>
2026-04-24 03:41:25 -0300

    feat: add update and delete endpoints for pasillera expenses in mobile API

--- routes/api.php
+++ routes/api.php
@@ -95,6 +95,8 @@ Route::prefix('mobile')->middleware('auth:sanctum')->group(function () {
     Route::prefix('pasillera')->group(function () {
         Route::get('/my-active', [MobilePasilleraController::class, 'getMyActivePasillera']);
         Route::post('/expense', [MobilePasilleraController::class, 'registerExpense']);
+        Route::put('/expense/{id}', [MobilePasilleraController::class, 'updateExpense']);
+        Route::delete('/expense/{id}', [MobilePasilleraController::class, 'deleteExpense']);
         Route::get('/history', [MobilePasilleraController::class, 'getExpenseHistory']);
         Route::get('/machines', [MobilePasilleraController::class, 'getMachines']);
         Route::get('/balance', [MobilePasilleraController::class, 'getBalance']);
